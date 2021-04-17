61bd7741d13948f51e1d74becc27b092e1eafbab
Okiemute Gold <goldokiemute@gmail.com>
2021-04-17 15:39:39 +0100

    fetched data from placeholder, result written to posts.json

---
+++ result/result.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const fetch = require('node-fetch');
+
+const jsonPlaceholderUrl = "http://jsonplaceholder.typicode.com/posts";
+let settings = { method: "Get" };
+
+fetch(jsonPlaceholderUrl, settings)
+    .then(res => res.json())
+    .then((data, err) => {
+        if (err) throw err;
+
+        const postsPath = path.join(__dirname, "posts.json");
+        fs.writeFile(postsPath, JSON.stringify(data, null, 2), function () {
+            console.log(data);
+        });
+    });
+    // .then((data) => {
+    //     let postsPath = path.join(__dirname, "posts.json");
+    //     fs.writeFile(postsPath, JSON.stringify(data, null, 2), function () {
+    //         console.log("Replaced!");
+    //         console.log(data);
+    //     });
+    // })
+    // .catch((err) => {
+    //     console.log(err);
+    // });
+
